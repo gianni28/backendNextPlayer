8d762e7f9d3a9ff5560c0e1e619cc6c0017e3ead
gianni28 <a.gianni2003@gmail.com>
2024-11-16 17:50:37 -0500

    Se modifican credenciales

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.post('/api/register', async (req, res) => {
 });
 
 // Iniciar servidor
-const PORT = 5000;
+const PORT = process.env.PORT || 5000; // Usa el puerto de Render o 5000 como fallback
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
